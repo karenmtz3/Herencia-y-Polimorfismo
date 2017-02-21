31e5d7ad07ba3c8eaf46196b4e6e7b97a50c58b5
karenmtz3 <karenmtz273@gmail.com>
2017-02-20 19:08:08 -0600

    Herencia y polimorfismo
    termino de los metodos para calcular el area de un cuadrado y un circulo

---
+++ herencia y polimorfismo/Cuadrado.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Cuadrado here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Cuadrado extends Figura
+{
+    private float lado;
+    
+    public Cuadrado(float l)
+    {
+        lado = l;
+    }
+    
+    public void CalculaArea()
+    {
+        area = lado * lado;
+    }
+    
+}
--- herencia y polimorfismo/Principal.java
+++ herencia y polimorfismo/Principal.java
@@ -12,5 +12,13 @@ public class Principal
         Triangulo t1 = new Triangulo(3,4);
         t1.CalculaArea();
         System.out.println("El area del triangulo es " + t1.accedeArea());
+        
+        Cuadrado c1 = new Cuadrado(5);
+        c1.CalculaArea();
+        System.out.println("El area del cuadrado es " + c1.accedeArea());
+        
+        Circulo ci1 = new Circulo(3);
+        ci1.CalculaArea();
+        System.out.println("El area del circulo es " + ci1.accedeArea());
     }
 }
